0318ef7d57ac3c0446afdcf20923ae7ced2aaea1
James Perez <james.perez@latimes.com>
2018-04-18 15:10:18 -0700

    Setup and configuration for pypi publishing

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyBNS",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'futures; python_version == "2.7"',
+        'future; python_version == "2.7"',
+        'urllib3; python_version == "2.7"'
+    ],
+    author="Vanessa Martinez",
+    author_email="datadesk@latimes.com",
+    description="A wrapper for the Bloomberg News Service API",
+    long_description="Allows python applications to easily connect and interact with the Bloomberg News Service API.",
+    url="https://github.com/datadesk/py-bns",
+    license="MIT License",
+)
